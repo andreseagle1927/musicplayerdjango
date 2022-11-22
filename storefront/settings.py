8bcc17592766b8f054dbb24b408c9c6d970360e6
andreseagle1927 <96150493+andreseagle1927@users.noreply.github.com>
2022-11-22 16:35:17 -0500

    Update settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c)mkicq5$mqv0hbo1%grun3aw7w)2j%0-48se%i@wpfd@84)%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['labsoft2022.herokuapp.com']
 
